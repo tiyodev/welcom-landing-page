f69402dd4addc71f6edc6b7d94952f38bd54b25c
Tiyo <tiyodev@gmail.com>
2017-09-28 11:48:44 +0200

    Handle favicon

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.set('view engine', 'pug');
 /**
  * Express configuration.
  */
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
